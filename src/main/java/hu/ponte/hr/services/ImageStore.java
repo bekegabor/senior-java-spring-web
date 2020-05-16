d848a3acba9004bd7ab395dcfada93f253856cb5
Gabor Beke <beke.gabor87@outlook.com>
2020-05-16 11:33:28 +0200

    Creating ImageStore Service methods

--- src/main/java/hu/ponte/hr/services/ImageStore.java
+++ src/main/java/hu/ponte/hr/services/ImageStore.java
@@ -1,8 +1,30 @@
 package hu.ponte.hr.services;
 
+import hu.ponte.hr.entity.ImageMeta;
+import hu.ponte.hr.repository.ImageMetaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Service
 public class ImageStore {
+
+    private final ImageMetaRepository imageMetaRepository;
+
+    @Autowired
+    public ImageStore(ImageMetaRepository imageMetaRepository) {
+        this.imageMetaRepository = imageMetaRepository;
+    }
+
+    public List<ImageMeta> listImages(){
+        return imageMetaRepository.findAll();
+    }
+
+    public void getImageById(String id, HttpServletResponse response){
+
+    }
+
 
 }
